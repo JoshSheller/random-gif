03b71bd17da0c63cbc765b9f9cd70a6a88eab91a
joshsheller <joshua.a.sheller@gmail.com>
2016-02-16 11:19:09 -0800

    Implement client search option to make different queries to giphy API

--- public/app.js
+++ public/app.js
@@ -1,9 +1,8 @@
 var app = angular.module('giphy', []);
 
 app.factory('giphyAPI', function($http) {
-
-  var collectGiphy = function() {
-    return $http.get('http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC')
+  var collectGiphy = function(keyword) {
+    return $http.get('http://api.giphy.com/v1/gifs/search?q=' + keyword + '&api_key=dc6zaTOxFJmzC')
     .then(function(res) {
       return res.data;
     });
@@ -14,11 +13,9 @@ app.factory('giphyAPI', function($http) {
   };
 });
 
-app.controller('findGiphy', function($scope, giphyAPI) {
-
-  $scope.data;
-  $scope.searchGiphyByName = function() {
-    giphyAPI.collectGiphy().then(function(data){
+app.controller('searchGiphy', function($scope, giphyAPI) {
+  $scope.searchGifs = function(keyword) {
+    giphyAPI.collectGiphy(keyword).then(function(data){
       $scope.data = data;
     });
   }
